22ee381afffe47479339174cad99af6a8df75642
nashbe26 <alatouatiii@gmail.com>
2024-04-19 14:38:10 +0100

    landing page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { User } from "./utils/recoil/atoms";
 import { useRecoilState } from "recoil";
 import { useGetUser } from "utils/functions";
@@ -35,8 +35,8 @@ const CategoryPage = React.lazy(() => import("pages/CategoryPage"));
 const ProductDetailPage = React.lazy(() => import("pages/ProductDetailPage"));
 const Pres = React.lazy(() => import("pages/Pres"));
 const Cart = React.lazy(() => import("pages/Cart"));
-const Two = React.lazy(() => import("pages/Two"));
-const Four = React.lazy(() => import("pages/Four"));
+
+const Contact = React.lazy(() => import("pages/LandingPage/contactMain"));
 const LandingpagesThree = React.lazy(() => import("pages/LandingpagesThree"));
 const LandingpageContact = React.lazy(() => import("pages/LandingpageContact"));
 const LandingpagesSeven = React.lazy(() => import("pages/LandingpagesSeven"));
@@ -48,25 +48,25 @@ const Dashboard = React.lazy(() => import("pages/Dashboard"));
 const PresorderPage = React.lazy(() => import("pages/Presorder"));
 
 const ProjectRoutes = () => {
-  const [loggedIn,setLoggedIn] = useState(null)
-  const checkvalidUser= useGetUser()
-  const [token,setToken] = useRecoilState(User);
+  const [loggedIn, setLoggedIn] = useState(null)
+  const checkvalidUser = useGetUser()
+  const [token, setToken] = useRecoilState(User);
   React.useEffect(() => {
-   
+
   }, [token]);
   React.useEffect(() => {
     const fetchUserFromToken = async () => {
-     if(!token){
-      setLoggedIn(false)
-      return
-     }
-     const user =  await checkvalidUser(token)
-     if(user.status!=401){
-     await setLoggedIn(true)
-     }else{
-      setLoggedIn(false)
-      return
-     }
+      if (!token) {
+        setLoggedIn(false)
+        return
+      }
+      const user = await checkvalidUser(token)
+      if (user.status != 401) {
+        await setLoggedIn(true)
+      } else {
+        setLoggedIn(false)
+        return
+      }
     };
 
     fetchUserFromToken();
@@ -74,48 +74,49 @@ const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
-      {loggedIn === null ? ( // Check if loggedIn state is null (initial state)
-        <div>Loading...</div> // Display a loading message while waiting for the state to update
-      ) : (
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<Landingpage />} />
-          {/* <Route path="/PresSignIn" element={<SignIn /> } /> */}
-          <Route path="/SignIn" element={<SignIn /> } />
-          <Route path="/PresSignIn" element={<PresSignIn /> } />
+        {loggedIn === null ? ( // Check if loggedIn state is null (initial state)
+          <div>Loading...</div> // Display a loading message while waiting for the state to update
+        ) : (
+          <Routes>
+            {/* Public routes */}
+            <Route path="/" element={<Landingpage />} />
+            {/* <Route path="/PresSignIn" element={<SignIn /> } /> */}
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/PresSignIn" element={<PresSignIn />} />
 
-          <Route path="/PresSignup" element={ <PresSignup />} />
+            <Route path="/PresSignup" element={<PresSignup />} />
 
-          <Route path="/Signup" element={ <Signup />} />
-          <Route path="*" element={<Navigate to="/SignIn" />} />
-       
-          <Route path="/Dashboard" element={loggedIn ? <Dashboard /> : <Navigate to="/Signup" />} />
-          <Route path="/presphoto/:id" element={<PresPHOTO />} />
-          <Route path="/landingpagesfour" element={<LandingpagesFour />} />
-          <Route path="/Landingpage" element={<Landingpage />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="*" element={<Navigate to="/SignIn" />} />
 
-          <Route path="/landingpagesfive" element={<LandingpagesFive />} />
-          <Route path="/landingpagessix" element={<LandingpagesSix />} />
-          <Route path="/landingpagesseven" element={<LandingpagesSeven />} />
-          <Route path="/landingpagesthree" element={<LandingpagesThree />} />
-          <Route path="/preslandingpage" element={<Preslandingpage />} />
+            <Route path="/Dashboard" element={loggedIn ? <Dashboard /> : <Navigate to="/Signup" />} />
+            <Route path="/presphoto/:id" element={<PresPHOTO />} />
+            <Route path="/landingpagesfour" element={<LandingpagesFour />} />
+            <Route path="/Landingpage" element={<Landingpage />} />
 
-          {/* Private routes */}
-          {loggedIn && (
-            <>
-                 <Route path="/homepage" element={ loggedIn ? <Homepage />  : <Navigate to="/Signup"  />} />
-          <Route path="/category" element={loggedIn ? <CategoryPage />  : <Navigate to="/Signup" /> } />
-          <Route path="/Checkout" element={ loggedIn ? <Checkout /> : <Navigate to="/Signup"  />} />
-          <Route path="/cart" element={loggedIn ?  <Cart />  : <Navigate to="/Signup"  />} /> 
-          <Route path="/productdetailpage/:id" element={loggedIn ?  <ProductDetailPage />  : <Navigate to="/Signup"  />} />
-             <Route path="/Dashboard" element={loggedIn ? <Dashboard /> : <Navigate to="/Signup" />} />
+            <Route path="/landingpagesfive" element={<LandingpagesFive />} />
+            <Route path="/landingpagessix" element={<LandingpagesSix />} />
+            <Route path="/landingpagesseven" element={<LandingpagesSeven />} />
+            <Route path="/landingpagesthree" element={<LandingpagesThree />} />
+            <Route path="/preslandingpage" element={<Preslandingpage />} />
+            <Route path="/contact" element={<Contact />} />
 
-            <Route path="/usersetting" element={loggedIn ?<USERSETTING />: <Navigate to="/Signup" />} />
+            {/* Private routes */}
+            {loggedIn && (
+              <>
+                <Route path="/homepage" element={loggedIn ? <Homepage /> : <Navigate to="/Signup" />} />
+                <Route path="/category" element={loggedIn ? <CategoryPage /> : <Navigate to="/Signup" />} />
+                <Route path="/Checkout" element={loggedIn ? <Checkout /> : <Navigate to="/Signup" />} />
+                <Route path="/cart" element={loggedIn ? <Cart /> : <Navigate to="/Signup" />} />
+                <Route path="/productdetailpage/:id" element={loggedIn ? <ProductDetailPage /> : <Navigate to="/Signup" />} />
+                <Route path="/Dashboard" element={loggedIn ? <Dashboard /> : <Navigate to="/Signup" />} />
 
-            </>
-          )}
-        </Routes>
-      )}
+                <Route path="/usersetting" element={loggedIn ? <USERSETTING /> : <Navigate to="/Signup" />} />
+
+              </>
+            )}
+          </Routes>
+        )}
       </Router>
     </React.Suspense>
     /*
